ecf2f0aabc563b8361a1cff7a86a6f04f6d24a42
Hirofumi Seiyama <hseiyama@gmail.com>
2026-01-15 22:39:09 +0900

    uno4_minima_register01: レジスタ操作のパターンを一部変更

--- Projects/uno4_minima_register01/src/main.cpp
+++ Projects/uno4_minima_register01/src/main.cpp
@@ -14,13 +14,19 @@ void loop() {
 	R_PORT0->PODR_b.PODR13 = 1;		// RX LED(P013): High出力(消灯)
 	delay(1000);
 	// 各ポートの出力データ設定(2)
-	R_PORT1->PODR_b.PODR11 = 0;		// SCK LED(P111): High出力(消灯)
-	R_PORT0->PODR_b.PODR12 = 0;		// TX LED(P012): High出力(点灯)
-	R_PORT0->PODR_b.PODR13 = 1;		// RX LED(P013): High出力(消灯)
+//	R_PORT1->PODR_b.PODR11 = 0;		// SCK LED(P111): High出力(消灯)
+//	R_PORT0->PODR_b.PODR12 = 0;		// TX LED(P012): High出力(点灯)
+//	R_PORT0->PODR_b.PODR13 = 1;		// RX LED(P013): High出力(消灯)
+	R_PORT1->PORR = 0x0800;
+	R_PORT0->PORR = 0x1000;
+	R_PORT0->POSR = 0x2000;
 	delay(1000);
 	// 各ポートの出力データ設定(3)
-	R_PORT1->PODR_b.PODR11 = 0;		// SCK LED(P111): High出力(消灯)
-	R_PORT0->PODR_b.PODR12 = 1;		// TX LED(P012): High出力(消灯)
-	R_PORT0->PODR_b.PODR13 = 0;		// RX LED(P013): High出力(点灯)
+//	R_PORT1->PODR_b.PODR11 = 0;		// SCK LED(P111): High出力(消灯)
+//	R_PORT0->PODR_b.PODR12 = 1;		// TX LED(P012): High出力(消灯)
+//	R_PORT0->PODR_b.PODR13 = 0;		// RX LED(P013): High出力(点灯)
+	R_PORT1->PORR_b.PORR11 = 1;
+	R_PORT0->POSR_b.POSR12 = 1;
+	R_PORT0->PORR_b.PORR13 = 1;
 	delay(1000);
 }
