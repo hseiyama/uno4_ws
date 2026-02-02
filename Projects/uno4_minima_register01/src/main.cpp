c79c20dc2653754e2241361feba5d027943ed4c7
Hirofumi Seiyama <hseiyama@gmail.com>
2026-02-03 05:51:12 +0900

    uno4_minima_register01: 外部割込み(PORT_IRQ0)を追加

--- Projects/uno4_minima_register01/src/main.cpp
+++ Projects/uno4_minima_register01/src/main.cpp
@@ -1,5 +1,11 @@
 #include <Arduino.h>
 
+#define FIXED_IRQ_NUM		(16)					// Arm Cortex-M4 コア用の固定IRQ数
+/* IRQ番号の割り当て */
+// IRQManager との競合を避けるため、
+// 末尾(31)から割り当てるルールとする。
+#define IRQ_PORT_IRQ0		(31)					// 外部端子割り込み(0)
+
 /*
  * SCI1 UART 初期化
  * 9600bps / 8bit / No parity / 1stop
@@ -65,6 +71,64 @@ char sci1_getc(void)
 	return R_SCI1->RDR;
 }
 
+#if 0
+/* 外部割込み処理 */
+static void sw_push(void)
+{
+	// 1文字送信
+	sci1_putc('e');
+}
+#endif
+
+/* IRQ0 割り込みハンドラ */
+void IRQ0_Handler(void)
+{
+    /* 割り込み要求フラグ クリア */
+	R_ICU->IELSR_b[IRQ_PORT_IRQ0].IR = 0;
+
+	// 1文字送信
+	sci1_putc('e');
+}
+
+/* IRQ0 初期化 */
+void irq0_init(void)
+{
+	/* ---- ベクターテーブル登録 ---- */
+	volatile uint32_t *irq_ptr = (volatile uint32_t *)SCB->VTOR;
+	irq_ptr += FIXED_IRQ_NUM;
+	__disable_irq();
+	*(irq_ptr + IRQ_PORT_IRQ0) = (uint32_t)IRQ0_Handler;
+	__enable_irq();
+
+	/* ---- IRQ0 無効 ---- */
+	R_ICU->IELSR[IRQ_PORT_IRQ0] = 0x00000000;
+	R_ICU->IRQCR[IRQ_PORT_IRQ0] = 0x00;
+
+	/* ---- ポート設定 ---- */
+	// 書き込みプロテクト解除
+	R_BSP_PinAccessEnable();
+	// P105 = IRQ0
+	R_PFS->PORT[1].PIN[5].PmnPFS_b.PMR = 0;			// 汎用入出力端子
+	R_PFS->PORT[1].PIN[5].PmnPFS_b.PCR = 1;			// 入力プルアップ有効
+	R_PFS->PORT[1].PIN[5].PmnPFS_b.ISEL = 1;		// IRQ0入力端子
+	// 書き込みプロテクト施錠
+	R_BSP_PinAccessDisable();
+
+	/* ---- IRQ0 設定 ---- */
+	R_ICU->IRQCR_b[IRQ_PORT_IRQ0].IRQMD = 0;		// 立ち下がりエッジ
+	R_ICU->IRQCR_b[IRQ_PORT_IRQ0].FCLKSEL = 3;		// PCLKB/64
+	R_ICU->IRQCR_b[IRQ_PORT_IRQ0].FLTEN = 1;		// デジタルフィルタ有効
+
+	/* ---- ICU → NVIC 割り込み割り当て ---- */
+	R_ICU->IELSR_b[IRQ_PORT_IRQ0].IR = 0;			// 割り込み要求フラグ クリア
+	R_ICU->IELSR_b[IRQ_PORT_IRQ0].IELS = 0x01;		// PORT_IRQ0
+
+	/* ---- NVIC 設定 ---- */
+	NVIC_ClearPendingIRQ((IRQn_Type)IRQ_PORT_IRQ0);
+	NVIC_SetPriority((IRQn_Type)IRQ_PORT_IRQ0, 12);
+	NVIC_EnableIRQ((IRQn_Type)IRQ_PORT_IRQ0);
+}
+
 void setup() {
 	// 各ポートの方向設定
 	R_PORT1->PDR_b.PDR11 = 1;		// SCK LED(P111): 出力
@@ -73,6 +137,14 @@ void setup() {
 
 	// SCI1 UART 初期化
 	sci1_init();
+
+#if 0
+	// 外部割込みの準備
+	pinMode(2, INPUT_PULLUP);
+	attachInterrupt(2, sw_push, FALLING);
+#endif
+	// IRQ0 初期化
+	irq0_init();
 }
 
 void loop() {
